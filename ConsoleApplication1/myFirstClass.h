d2f20326a694df769e13c6cb3477e71b78ee46bb
高金磊 <g1587613421@outlook.com>
2020-05-19 09:09:46 +0800

    study

--- ConsoleApplication1/myFirstClass.h
+++ ConsoleApplication1/myFirstClass.h
@@ -1,13 +1,34 @@
-
+#include <string.h>
+#include "string"
+using namespace std;
 //类声明
 class myFirstClass
 {
+	string tag;
+public: myFirstClass() {
+	tag = "name";
+	}
+	myFirstClass(string &name) {
+		tag=name;
+		printf("正在执行构造函数,tag为%s\n",name);
+	}
+
+	~myFirstClass()
+	{
+		printf("%s正在执行析构函数", tag);
+	}
 	//以下功能类似于接口--实现可以在这个类也可以在其他地方
 public:
 	int fo(int num);
 	int fo1(int num,int num1);
 	int fo1(int num,int num1,int num2);
+	int fo3() {
+		return 0;
+		//return "这个方法是myFirstClass内部定义的";
+	}
 private:
 	int fo2(int num, int num1);
 };
+
+
 
--- ConsoleApplication1/oobMain.cpp
+++ ConsoleApplication1/oobMain.cpp
@@ -1,3 +1,35 @@
+#include "myFirstClass.h"
+#include <iostream>
+#include <string.h>
+using namespace std;
+int myFirstClass::fo2(int num, int num1) {
+	return num + num1;
+}
+int myFirstClass::fo(int num) {
+	return fo2(num, 0);
+}
+int myFirstClass::fo1(int num, int num1) {
+	return fo2(num,num1);
+}
+int myFirstClass::fo1(int num, int num1, int num2) {
+	return fo2(fo2(num, num1), num2);
+}
+
+//由于fo3已经有定义所以这里不能再定义
+//int myFirstClass::fo3() {
+//	return 3;
+//	//return "这个方法已经被主函数修改";
+//}
 int main() {
+	//myFirstClass *my = new myFirstClass();
+	//cout<< my->fo1(1, 2);
+	////cout << my->fo2(1, 2);//不可达
+	//cout << my->fo(1);
+	
+//构造函数和析构函数
+	string name = "gjl";
+	myFirstClass *mtclass = new myFirstClass(name);
+	delete(mtclass);
+
 
 }
