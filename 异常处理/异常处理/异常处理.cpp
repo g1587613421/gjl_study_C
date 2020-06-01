3fafbb90bb0bea68e361eefd3c1b0ff63dca1517
高金磊 <g1587613421@outlook.com>
2020-06-01 15:31:12 +0800

    i

--- 异常处理/异常处理/异常处理.cpp
+++ 异常处理/异常处理/异常处理.cpp
@@ -1,46 +1,73 @@
 ﻿using namespace std;
 #include <iostream>
 
-int main()
-{
-    //简单的异常
-mm:    try
+
+
+//重新抛出异常
+void 重新抛出异常() {
+	try
 	{
-		int i;
-		cin >> i;
-		throw i;
+		throw 1;
 	}
 	catch (int exc)
 	{
-		if (exc == 1) {
-			cout << "1是我能捕获的异常";
-		}
-		else
+		try
 		{
-			try
-			{
-throw exc;
-			}
-			catch (int exc) {
-				if (exc ==2) {
-					cout << exc << "是我能捕获的异常";
-				}
-				else
-				{
-					try
-					{
-throw exc;
-					}
-					catch (...) {//捕获所有异常
-						cout << "异常没有被正常捕获" << "请重新输入异常标识符";
-						goto mm;
-					}
-					
-				}
-			}
-			
+throw;
 		}
+		catch (...) {
+			cout << "在这里被捕获了";
+		}
+		
 	}
 	
 }
+
+
+void 一般异常处理()
+{
+	//简单的异常
+mm:    try
+{
+	int i;
+	cin >> i;
+	throw i;
+}
+	   catch (int exc)
+	   {
+		   if (exc == 1) {
+			   cout << "1是我能捕获的异常";
+		   }
+		   else
+		   {
+			   try
+			   {
+				   throw exc;
+			   }
+			   catch (int exc) {
+				   if (exc == 2) {
+					   cout << exc << "是我能捕获的异常";
+				   }
+				   else
+				   {
+					   try
+					   {
+						   throw exc;
+					   }
+					   catch (...) {//捕获所有异常
+						   cout << "异常没有被正常捕获" << "请重新输入异常标识符";
+						   goto mm;
+					   }
+
+				   }
+			   }
+
+		   }
+	   }
+}
+
+	int main(){
+		//一般异常处理();
+		重新抛出异常();
+	}
 
