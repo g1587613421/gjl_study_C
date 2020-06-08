7a3770f0668a6113d54acd0c3a98a4f35805ec9f
高金磊 <g1587613421@outlook.com>
2020-06-08 12:25:41 +0800

    init

--- 二分搜索/二分搜索/二分搜索.cpp
+++ 二分搜索/二分搜索/二分搜索.cpp
@@ -2,19 +2,34 @@
 //
 
 #include <iostream>
+using namespace std;
 
+static int find(int nums[], int key,int size) {
+	int start = 0;
+	int last = size;
+	int middle;
+	while (last>=start)
+	{
+		middle = (start + last) / 2;
+		if (nums[middle]==key)
+		{
+			return middle;
+		}
+		if (nums[middle]>key)
+		{
+			last = middle-1;
+		}
+		else
+		{
+			start = middle+1;
+		}
+		
+	}
+
+	return -1;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	int data[] ={1,3,5,8,10,111 };
+	cout<<find(data,111, sizeof(data) / sizeof(*data));
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
